cb6ff4ab8d677819f5dbfa056583ccbde0ee7cc5
nazimadda <nazim.adda@fmxsolutions.com>
2024-09-16 09:55:40 -0400

    Restart After Changes Added

--- TP1/ViewModels/ConfigurationViewModel.cs
+++ TP1/ViewModels/ConfigurationViewModel.cs
@@ -13,6 +13,16 @@ namespace TP1.ViewModels
     public class ConfigurationViewModel : BaseViewModel
     {
 
+        private bool _isRestartChecked;
+        public bool IsRestartChecked
+        {
+            get { return _isRestartChecked; } 
+            set {
+                _isRestartChecked = value;
+                OnPropertyChanged();
+            }
+        }
+
         private string _selectedLanguage;
         protected CloseConfigurationWIndow _closeWindow;
 
@@ -31,16 +41,27 @@ namespace TP1.ViewModels
             {
                 return SelectedLanguage != null;
             });
+            CancelConfigurationCmd = new RelayCommand(CancelConfiguration, null);
         }
 
         public void SaveConfiguration(object? parameter)
         {
             TP1.Properties.Settings.Default.langue = SelectedLanguage;
             TP1.Properties.Settings.Default.Save();
+
+            if (IsRestartChecked) {
+                System.Diagnostics.Process.Start(fileName: Environment.ProcessPath);
+                Application.Current.Shutdown();
+            }
             _closeWindow();
 
         }
+        public void CancelConfiguration(object? parameter)
+        {
+            _closeWindow();
+        }
 
         public RelayCommand SaveConfigurationCmd { get; set; }
+        public RelayCommand CancelConfigurationCmd { get; set; }
     }
 }
