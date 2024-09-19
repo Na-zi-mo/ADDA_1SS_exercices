143959aea60c39fee9118785a78e23baa4bc883e
nazimadda <nazim.adda@fmxsolutions.com>
2024-09-18 23:05:52 -0400

    Language ComboBox has the correct language selected

--- TP1/ViewModels/ConfigurationViewModel.cs
+++ TP1/ViewModels/ConfigurationViewModel.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Data;
 using TP1.ViewModels.Commands;
 using static TP1.ViewModels.Delegates.ViewModelDelegates;
@@ -23,21 +25,25 @@ namespace TP1.ViewModels
             }
         }
 
-        private string _selectedLanguage;
+        
         protected CloseConfigurationWIndow _closeWindow;
         protected ShowInformationDialog _showInformation;
 
+        private string _selectedLanguage;
         public string SelectedLanguage
         {
             get { return _selectedLanguage; }
             set {
                 _selectedLanguage = value;
+                OnPropertyChanged(nameof(SelectedLanguage));
             } 
         }
         public ConfigurationViewModel(CloseConfigurationWIndow closeWindow, ShowInformationDialog showInformation) 
         {
             _closeWindow = closeWindow;
             _showInformation = showInformation;
+
+            SelectedLanguage = TP1.Properties.Settings.Default.langue == "en-US" ? "en-US" : "fr-CA";
 
             SaveConfigurationCmd = new RelayCommand(SaveConfiguration, (object? parameter) =>
             {
