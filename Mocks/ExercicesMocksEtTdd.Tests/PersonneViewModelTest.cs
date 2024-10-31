e95c8371c6e39362b5ef0d49feb658189f9923d5
Na-zi-mo <nazimad2004@gmail.com>
2024-10-31 17:14:46 -0400

    Exercice 1 Done

--- Mocks/ExercicesMocksEtTdd.Tests/PersonneViewModelTest.cs
+++ Mocks/ExercicesMocksEtTdd.Tests/PersonneViewModelTest.cs
@@ -42,11 +42,199 @@ namespace ExercicesMocksEtTdd.Tests
         private List<Personne> ListePersonnesAttendues()
         {
             return new List<Personne>()
+            {
+                new Personne(1, "Carnaval", "Bonhomme", "418-123-4567"),
+                new Personne(2, "Gratton", "Bob", "450-659-8854"),
+                new Personne(3, "Troudeau", "Justun", "514-465-4785")
+            };
+        }
+
+        [Fact]
+        public void AjouterPersonne_ShouldBeAdded()
         {
-            new Personne(1, "Carnaval", "Bonhomme", "418-123-4567"),
-            new Personne(2, "Gratton", "Bob", "450-659-8854"),
-            new Personne(3, "Troudeau", "Justun", "514-465-4785")
-        };
+            // Préparation (le mock est déjà configuré pour vous ici pour vous aider à démarrer...)
+            _pDataService.Setup(pds => pds.GetAll()).Returns(ListePersonnesAttendues());
+            _pDataService.Setup(pds => pds.Insert(It.IsAny<Personne>())).Returns(true);
+            // Suite préparation
+            PersonneViewModel pvm = new PersonneViewModel(_interUtilMock.Object, _pDataService.Object);
+
+            // Exécution (l'exécution est déjà fournie pour vous aidez à démarrer...)
+            pvm.Nom = "Massinissa";
+            pvm.Prenom = "Massyla";
+            pvm.Telephone = "(123) 456-7890";
+
+            pvm.AjouterPersonne(null);
+
+            // Affirmation
+            Assert.Equal(4, pvm.Personnes.Count());
+        }
+
+        [Fact]
+        public void AjouterPersonne_ShouldNotBeAdded()
+        {
+            // Préparation (entre autres, configurer le mock...)
+            _pDataService.Setup(pds => pds.GetAll()).Returns(ListePersonnesAttendues());
+            _pDataService.Setup(pds => pds.Insert(It.IsAny<Personne>())).Returns(true);
+            // Suite préparation
+            PersonneViewModel pvm = new PersonneViewModel(_interUtilMock.Object, _pDataService.Object);
+
+
+            // Exécution
+            pvm.AjouterPersonne(null);
+
+
+            // Affirmation
+            Assert.Equal(3, pvm.Personnes.Count());
+        }
+
+        [Fact]
+        public void SupprimerTout_ShouldBeDeleted()
+        {
+            // Préparation
+            _pDataService.Setup(pds => pds.GetAll()).Returns(ListePersonnesAttendues());
+            _pDataService.Setup(pds => pds.Insert(It.IsAny<Personne>())).Returns(true);
+            // Suite préparation
+            PersonneViewModel pvm = new PersonneViewModel(_interUtilMock.Object, _pDataService.Object);
+
+            // Exécution
+            pvm.SupprimerTout(null);
+
+            // Affirmation
+            Assert.Equal(0, pvm.Personnes.Count());
+        }
+
+        [Fact]
+        public void SupprimerPersonne_ShouldBeDeleted()
+        {
+            // Préparation
+            _pDataService.Setup(pds => pds.GetAll()).Returns(ListePersonnesAttendues());
+            _pDataService.Setup(pds => pds.Insert(It.IsAny<Personne>())).Returns(true);
+            // Suite préparation
+            PersonneViewModel pvm = new PersonneViewModel(_interUtilMock.Object, _pDataService.Object);
+
+            // Exécution
+
+            pvm.PersonneSelectionnee = pvm.Personnes.Last();
+
+
+            pvm.SupprimerPersonne(null);
+
+            // Affirmation
+            Assert.Equal(2, pvm.Personnes.Count());
+        }
+
+        [Fact]
+        public void SupprimerPersonne_ShouldNotBeDeleted()
+        {
+            // Préparation
+            _pDataService.Setup(pds => pds.GetAll()).Returns(ListePersonnesAttendues());
+            _pDataService.Setup(pds => pds.Insert(It.IsAny<Personne>())).Returns(true);
+            // Suite préparation
+            PersonneViewModel pvm = new PersonneViewModel(_interUtilMock.Object, _pDataService.Object);
+
+            // Exécution
+            pvm.SupprimerPersonne(null);
+
+            // Affirmation
+            Assert.Equal(3, pvm.Personnes.Count());
+        }
+
+        [Fact]
+        public void ModifierPersonne_ShouldBeModified()
+        {
+            // Préparation
+            _pDataService.Setup(pds => pds.GetAll()).Returns(ListePersonnesAttendues());
+            _pDataService.Setup(pds => pds.Insert(It.IsAny<Personne>())).Returns(true);
+            // Suite préparation
+            PersonneViewModel pvm = new PersonneViewModel(_interUtilMock.Object, _pDataService.Object);
+
+            pvm.PersonneSelectionnee = pvm.Personnes.Last();
+
+            pvm.Nom = "Massinissa";
+            pvm.Prenom = "Massyla";
+            pvm.Telephone = "(123) 456-7890";
+
+            // Exécution
+            pvm.ModifierPersonne(null);
+
+            // Affirmation
+            Assert.Equal("Massinissa", pvm.Personnes.Last().Nom);
+        }
+
+        [Fact]
+        public void ModifierPersonne_ShouldNotBeModified()
+        {
+            // Préparation
+            _pDataService.Setup(pds => pds.GetAll()).Returns(ListePersonnesAttendues());
+            _pDataService.Setup(pds => pds.Insert(It.IsAny<Personne>())).Returns(true);
+            // Suite préparation
+            PersonneViewModel pvm = new PersonneViewModel(_interUtilMock.Object, _pDataService.Object);
+
+            pvm.PersonneSelectionnee = null;
+
+            pvm.Nom = "Massinissa";
+            pvm.Prenom = "Massyla";
+            pvm.Telephone = "(123) 456-7890";
+
+            // Exécution
+            pvm.ModifierPersonne(null);
+
+            // Affirmation
+            Assert.NotEqual("Massinissa", pvm.Personnes.Last().Nom);
+        }
+
+        [Fact]
+        public void AnnulerPersonne_ShouldBeCancelled()
+        {
+            // Préparation
+            _pDataService.Setup(pds => pds.GetAll()).Returns(ListePersonnesAttendues());
+            _pDataService.Setup(pds => pds.Insert(It.IsAny<Personne>())).Returns(true);
+            // Suite préparation
+            PersonneViewModel pvm = new PersonneViewModel(_interUtilMock.Object, _pDataService.Object);
+
+            pvm.PersonneSelectionnee = pvm.Personnes.First();
+
+            pvm.Nom = pvm.Personnes.First().Nom;
+            pvm.Prenom = pvm.Personnes.First().Prenom;
+            pvm.Telephone = pvm.Personnes.First().Telephone;
+
+            // Exécution
+            pvm.Annuler(null);
+
+            // Affirmation
+            Assert.True(pvm.ModeAjout);
+        }
+
+        [Fact]
+        public void SetPersonneSelectionnee_ShouldBeSelected()
+        {
+            // Préparation
+            _pDataService.Setup(pds => pds.GetAll()).Returns(ListePersonnesAttendues());
+            _pDataService.Setup(pds => pds.Insert(It.IsAny<Personne>())).Returns(true);
+            // Suite préparation
+            PersonneViewModel pvm = new PersonneViewModel(_interUtilMock.Object, _pDataService.Object);
+
+            // Exécution
+            pvm.PersonneSelectionnee = pvm.Personnes.First();
+
+            // Affirmation
+            Assert.NotNull(pvm.PersonneSelectionnee);
+        }
+
+        [Fact]
+        public void SetPersonneSelectionnee_ShouldNotBeSelected()
+        {
+            // Préparation
+            _pDataService.Setup(pds => pds.GetAll()).Returns(ListePersonnesAttendues());
+            _pDataService.Setup(pds => pds.Insert(It.IsAny<Personne>())).Returns(true);
+            // Suite préparation
+            PersonneViewModel pvm = new PersonneViewModel(_interUtilMock.Object, _pDataService.Object);
+
+            // Exécution
+            pvm.PersonneSelectionnee = null;
+
+            // Affirmation
+            Assert.Null(pvm.PersonneSelectionnee);
         }
     }
 }
