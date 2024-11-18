eeea79067743123ac0b6a8e9e415e69f7204cbb1
Na-zi-mo <nazimad2004@gmail.com>
2024-11-18 09:49:40 -0500

    Added Project

---
+++ ExerciceEfCore/ExerciceEfCore/Entities/Blog.cs
@@ -0,0 +1,15 @@
+﻿namespace ExerciceEfCore.Entities
+{
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Url { get; set; }
+        public List<Post> Posts { get; set; } = new();
+
+        public override string ToString()
+        {
+            return $"BlogId #{BlogId}, Url : {Url}\n============\n"
+                + string.Join("\n============\n", Posts);
+        }
+    }
+}
---
+++ ExerciceEfCore/ExerciceEfCore/Entities/Post.cs
@@ -0,0 +1,16 @@
+﻿namespace ExerciceEfCore.Entities
+{
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int BlogId { get; set; }
+        public Blog Blog { get; set; }
+
+        public override string ToString()
+        {
+            return $"PostId #{PostId}\nBlogId #{BlogId}\nTitre : {Title}\nContenu : {Content}";
+        }
+    }
+}
