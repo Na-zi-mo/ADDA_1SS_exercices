c8ecc1b2c27d028c33780c227f995c68ec44edaf
Na-zi-mo <nazimad2004@gmail.com>
2024-11-18 08:45:31 -0500

    Added Repositories

---
+++ ExempleEfCore/JardinageWpf/DataService/Repositories/Interfaces/IPlanteRepository.cs
@@ -0,0 +1,24 @@
+﻿using JardinageWpf.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JardinageWpf.DataService.Repositories.Interfaces
+{
+    public interface IPlanteRepository
+    {
+        Plante? Get(int id);
+
+        List<Plante> GetAll();
+
+        Task<List<Plante>> GetAllAsync();
+
+        Task<Plante> AddAsync(Plante plante);
+
+        Task<Plante> UpdateAsync(Plante plante);
+
+        Task<bool> DeleteAsync(Plante plante);
+    }
+}
