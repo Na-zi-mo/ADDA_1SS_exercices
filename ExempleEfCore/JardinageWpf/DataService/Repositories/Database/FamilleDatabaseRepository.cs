fd43b92b50fef174967622357ee0445204a52ea4
Na-zi-mo <nazimad2004@gmail.com>
2024-11-18 08:47:56 -0500

    Added Database Repositories

---
+++ ExempleEfCore/JardinageWpf/DataService/Repositories/Database/FamilleDatabaseRepository.cs
@@ -0,0 +1,26 @@
+﻿using JardinageWpf.DataService.Repositories.Interfaces;
+using JardinageWpf.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JardinageWpf.DataService.Repositories.Database
+{
+    public class FamilleDatabaseRepository : BaseDatabaseRepository, IFamilleRepository
+    {
+        public FamilleDatabaseRepository(JardinageDbContext context) : base(context) { }
+
+        public List<Famille> GetAll()
+        {
+            return _context.Familles.OrderBy(f => f.Nom).ToList();
+        }
+
+        public async Task<List<Famille>> GetAllAsync()
+        {
+            return await _context.Familles.OrderBy(f => f.Nom).ToListAsync();
+        }
+    }
+}
