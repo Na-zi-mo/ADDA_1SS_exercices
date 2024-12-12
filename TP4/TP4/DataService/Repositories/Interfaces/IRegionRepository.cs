0cb2cf198c51c9a60bcbf65f4aeb4b0baac82eee
Na-zi-mo <nazimad2004@gmail.com>
2024-12-11 21:24:45 -0500

    Added Repository Interface

---
+++ TP4/TP4/DataService/Repositories/Interfaces/IRegionRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using TP4.Models;
+
+namespace TP4.DataService.Repositories.Interfaces
+{
+    public interface IRegionRepository
+    {
+        Task<Region?> GetAsync(int id);
+
+        Task<List<Region>> GetAllAsync();
+
+        Task<Region> AddAsync(Region region);
+
+        Task<bool> DeleteAsync(Region region);
+    }
+}
