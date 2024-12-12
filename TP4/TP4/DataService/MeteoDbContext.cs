5fb08fb1483303c13d2f9c0c00eec18a901f9759
Na-zi-mo <nazimad2004@gmail.com>
2024-12-12 02:06:15 -0500

    Added Migrations And Displaying Previsions

--- TP4/TP4/DataService/MeteoDbContext.cs
+++ TP4/TP4/DataService/MeteoDbContext.cs
@@ -18,8 +18,23 @@ namespace TP4.DataService
         {
             List<Region> regions = new List<Region>()
             {
-                new Region{ Id = 1, Nom = "Shawinigan", Latitude = 46.56984172477484, Longitude =  -72.73811285651442 },
-                new Region{ Id = 2, Nom = "Algiers", Latitude = 36.7538, Longitude = 3.0588 }
+                new Region{ Id = 1, Nom = "Shawinigan", Latitude = 46.56984172477484, Longitude = -72.73811285651442 },
+                new Region{ Id = 2, Nom = "Algiers", Latitude = 36.7538, Longitude = 3.0588 },
+                new Region{ Id = 3, Nom = "Oran", Latitude = 35.6971, Longitude = -0.6308 },
+                new Region{ Id = 4, Nom = "Constantine", Latitude = 36.365, Longitude = 6.6147 },
+                new Region{ Id = 5, Nom = "Annaba", Latitude = 36.9, Longitude = 7.766 },
+                new Region{ Id = 6, Nom = "Blida", Latitude = 36.4701, Longitude = 2.8277 },
+                new Region{ Id = 7, Nom = "Batna", Latitude = 35.555, Longitude = 6.1744 },
+                new Region{ Id = 8, Nom = "Tlemcen", Latitude = 34.8783, Longitude = -1.315 },
+                new Region{ Id = 9, Nom = "Sétif", Latitude = 36.192, Longitude = 5.4138 },
+                new Region{ Id = 10, Nom = "Béjaïa", Latitude = 36.7509, Longitude = 5.0566 },
+                new Region{ Id = 11, Nom = "Ghardaïa", Latitude = 32.4908, Longitude = 3.6735 },
+                new Region{ Id = 12, Nom = "Tizi Ouzou", Latitude = 36.717, Longitude = 4.0459 },
+                new Region{ Id = 13, Nom = "Skikda", Latitude = 36.8662, Longitude = 6.9067 },
+                new Region{ Id = 14, Nom = "Biskra", Latitude = 34.8504, Longitude = 5.728 },
+                new Region{ Id = 15, Nom = "Adrar", Latitude = 27.8761, Longitude = -0.2832 },
+                new Region{ Id = 16, Nom = "Laghouat", Latitude = 33.8036, Longitude = 2.8829 },
+                new Region{ Id = 17, Nom = "Tamanrasset", Latitude = 22.785, Longitude = 5.5228 }
             };
 
             modelBuilder.Entity<Region>().HasData(regions);
--- TP4/TP4/Migrations/20241212064522_CreationInitialeBdSqlite.cs
+++ TP4/TP4/Migrations/20241212064522_CreationInitialeBdSqlite.cs
@@ -2,6 +2,8 @@
 
 #nullable disable
 
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
 namespace TP4.Migrations
 {
     /// <inheritdoc />
@@ -28,7 +30,26 @@ namespace TP4.Migrations
             migrationBuilder.InsertData(
                 table: "Regions",
                 columns: new[] { "Id", "Latitude", "Longitude", "Nom" },
-                values: new object[] { 1, 46.56984172477484, -72.738112856514419, "Shawinigan " });
+                values: new object[,]
+                {
+                    { 1, 46.56984172477484, -72.738112856514419, "Shawinigan" },
+                    { 2, 36.753799999999998, 3.0588000000000002, "Algiers" },
+                    { 3, 35.697099999999999, -0.63080000000000003, "Oran" },
+                    { 4, 36.365000000000002, 6.6147, "Constantine" },
+                    { 5, 36.899999999999999, 7.766, "Annaba" },
+                    { 6, 36.470100000000002, 2.8277000000000001, "Blida" },
+                    { 7, 35.555, 6.1744000000000003, "Batna" },
+                    { 8, 34.878300000000003, -1.3149999999999999, "Tlemcen" },
+                    { 9, 36.192, 5.4138000000000002, "Sétif" },
+                    { 10, 36.750900000000001, 5.0566000000000004, "Béjaïa" },
+                    { 11, 32.4908, 3.6735000000000002, "Ghardaïa" },
+                    { 12, 36.716999999999999, 4.0458999999999996, "Tizi Ouzou" },
+                    { 13, 36.866199999999999, 6.9066999999999998, "Skikda" },
+                    { 14, 34.8504, 5.7279999999999998, "Biskra" },
+                    { 15, 27.876100000000001, -0.28320000000000001, "Adrar" },
+                    { 16, 33.803600000000003, 2.8828999999999998, "Laghouat" },
+                    { 17, 22.785, 5.5228000000000002, "Tamanrasset" }
+                });
 
             migrationBuilder.CreateIndex(
                 name: "IX_Regions_Nom",
