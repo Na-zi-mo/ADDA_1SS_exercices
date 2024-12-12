59f58e823027fcef3fa5c1dbe67a9dceccad347d
Na-zi-mo <nazimad2004@gmail.com>
2024-12-11 21:42:42 -0500

    Implemented Repository Interface

---
+++ TP4/TP4/DataService/Repositories/Database/BaseDatabaseRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP4.DataService.Repositories.Database
+{
+    public class BaseDatabaseRepository 
+    {
+        protected readonly MeteoDbContext _context;
+        protected BaseDatabaseRepository(MeteoDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
---
+++ TP4/TP4/DataService/Repositories/Database/RegionDatabaseRepository.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TP4.DataService.Repositories.Interfaces;
+using TP4.Models;
+
+namespace TP4.DataService.Repositories.Database
+{
+    public class RegionDatabaseRepository : BaseDatabaseRepository, IRegionRepository
+    {
+        public RegionDatabaseRepository(MeteoDbContext context) : base(context) { }
+
+        public async Task<Region?> GetAsync(int id)
+        {
+            return await _context.Regions
+                .FindAsync(id);
+        }
+
+        public async Task<List<Region>> GetAllAsync()
+        {
+            return await _context.Regions
+                .ToListAsync();
+        }
+
+        public async Task<Region> AddAsync(Region region)
+        {
+            _context.Regions.Add(region);
+            await _context.SaveChangesAsync();
+            return region;
+        }
+
+        public async Task<bool> DeleteAsync(Region region)
+        {
+            _context.Regions.Remove(region);
+            await _context.SaveChangesAsync();
+            return true;
+        }        
+    }
+}
