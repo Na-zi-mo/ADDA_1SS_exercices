39c2985cdf9bce418741f7727252a85f2eed74d6
Na-zi-mo <nazimad2004@gmail.com>
2024-12-12 19:37:43 -0500

    Working on Unit Test

--- TP4/TP4.Tests/MeteoViewModelTest.cs
+++ TP4/TP4.Tests/MeteoViewModelTest.cs
@@ -1,7 +1,21 @@
+using Moq;
+using TP4.DataService.Repositories.Interfaces;
+using TP4.ViewModels.Interfaces;
+
 namespace TP4.Tests
 {
     public class MeteoViewModelTest
     {
+        private readonly Mock<IInteractionUtilisateur> _interactionUtilisateurMock= new Mock<IInteractionUtilisateur>();
+        private readonly Mock<IRegionRepository> _regionRepositoryMock = new Mock<IRegionRepository>();
+
+
+        public MeteoViewModelTest()
+        {
+            _interactionUtilisateurMock.Setup(iu => iu.ShowErrorMessage(It.IsAny<string>()));
+            _interactionUtilisateurMock.Setup(iu => iu.ShowInformationMessage(It.IsAny<string>()));
+        }
+
         [Fact]
         public void Test1()
         {
